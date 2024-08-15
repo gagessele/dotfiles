4bbcea79ec30f9c9bec608985e76509a83ab9a25
Gui Gessele <gagessele@icloud.com>
2024-08-15 08:28:52 -0300

    update emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,14 +2,20 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
-(unless (package-installed-p 'paredit)
-(package-install 'paredit))
+(package-initialize)
 
-(unless (package-installed-p 'cider)
-  (package-install 'cider))
+(setq package-selected-packages '(cider
+				  clojure-mode
+				  corfu
+				  exec-path-from-shell
+				  eglot
+				  magit
+				  paredit
+				  vertico))
 
-(unless (package-installed-p 'clojure-mode)
-  (package-install 'clojure-mode))
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
 
 (autoload 'enable-paredit-mode "paredit"
   "Turn on pseudo-structural editing of Lisp code."
@@ -20,12 +26,37 @@
 
 (setq cider-save-file-on-load t)
 
-(unless (package-installed-p 'magit)
-  (package-install 'magit))
-
-(unless (package-installed-p 'exec-path-from-shell)
-  (package-install 'exec-path-from-shell))
 (exec-path-from-shell-initialize)
+
+;; Enable vertico
+(use-package vertico
+  :init
+  (vertico-mode))
+
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; Enable corfu
+(use-package corfu
+  :init
+  (global-corfu-mode))
+
+;; Emacs configuration
+(use-package emacs
+  :custom
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point` is often bound to M-TAB.
+  (tab-always-indent 'complete)
+
+  ;; Emacs 30 and newer: Disable Ispell completion function. As an alternative,
+  ;; try `cape-dict'.
+  (text-mode-ispell-word-completion nil)
+
+  ;; Emacs 28 and newer: Hide commands in M-x which do not apply to the current
+  ;; mode.  Corfu commands are hidden, since they are not used via M-x. This
+  ;; setting is useful beyond Corfu.
+  (read-extended-command-predicate #'command-completion-default-include-p))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -42,5 +73,5 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 150 :width normal :foundry "nil" :family "Menlo")))))
 
+;; startup initial frame maximized
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
