85e1689341428058e9e4ae6ac791eb1c3936ba30
Gui Gessele <gagessele@icloud.com>
2024-08-16 13:21:46 -0300

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,77 +1,23 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-
-(package-initialize)
-
-(setq package-selected-packages '(cider
-				  clojure-mode
-				  corfu
-				  exec-path-from-shell
-				  eglot
-				  magit
-				  paredit
-				  vertico))
-
-(when (cl-find-if-not #'package-installed-p package-selected-packages)
-  (package-refresh-contents)
-  (mapc #'package-install package-selected-packages))
-
-(autoload 'enable-paredit-mode "paredit"
-  "Turn on pseudo-structural editing of Lisp code."
-  t)
-(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'clojure-mode-hook 'enable-paredit-mode)
-(add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
-
-(setq cider-save-file-on-load t)
-
-(exec-path-from-shell-initialize)
-
-;; Enable vertico
-(use-package vertico
-  :init
-  (vertico-mode))
-
-(use-package savehist
-  :init
-  (savehist-mode))
-
-;; Enable corfu
-(use-package corfu
-  :init
-  (global-corfu-mode))
-
-;; Emacs configuration
 (use-package emacs
-  :custom
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point` is often bound to M-TAB.
-  (tab-always-indent 'complete)
+  :init
+  ;; startup initial frame maximized
+  (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+  (set-face-attribute 'default nil :height 240)
+  (setq global-display-line-numners-mode t))
 
-  ;; Emacs 30 and newer: Disable Ispell completion function. As an alternative,
-  ;; try `cape-dict'.
-  (text-mode-ispell-word-completion nil)
+(use-package clojure-mode
+  :ensure t)
 
-  ;; Emacs 28 and newer: Hide commands in M-x which do not apply to the current
-  ;; mode.  Corfu commands are hidden, since they are not used via M-x. This
-  ;; setting is useful beyond Corfu.
-  (read-extended-command-predicate #'command-completion-default-include-p))
+(use-package cider
+  :ensure t
+  :config
+  (setq cider-save-file-on-load t)
+  (setq cider-repl-display-help-banner nil))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(display-line-numbers t)
- '(package-selected-packages '(magit cider)))
+(use-package magit
+  :ensure t)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 150 :width normal :foundry "nil" :family "Menlo")))))
-
-;; startup initial frame maximized
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(use-package paredit
+  :ensure t
+  :after (clojure-mode cider)
+  :hook (emacs-lisp-mode clojure-mode cider-repl-mode))
