0221b323c483b33ce6fd5e97c9f10353fb428461
Gui Gessele <gagessele@icloud.com>
2024-09-05 09:22:01 -0300

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,24 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(setq package-selected-packages '(cider
+				  clojure-mode
+				  corfu
+				  flycheck
+				  flycheck-clj-kondo
+				  go-mode
+				  magit
+				  marginalia
+				  orderless
+				  paredit
+				  vertico))
+
+
+(package-initialize)
+
 (use-package emacs
   :custom
   ;; Support opening new minibuffers from inside existing minibuffers.
@@ -15,15 +36,32 @@
   (set-face-attribute 'default nil :height 180)
   (global-display-line-numbers-mode 1)
   (setq backup-directory-alist '(("." . "~/.emacs.d/backups"))
+	auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t))
 	backup-by-copying t
 	delete-old-versions t
 	inhibit-splash-screen t
 	kept-new-versions 6
 	kept-old-versions 2
-	version-control t))
+	version-control t)
+  (make-directory "~/.emacs.d/autosaves/" t))
+
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode))
+
+(use-package flycheck-clj-kondo
+  :ensure t)
 
 (use-package clojure-mode
-  :ensure t)
+  :ensure t
+  :config
+  (require 'flycheck-clj-kondo))
+
+;; (use-package clojure-ts-mode
+;;   :ensure t
+;;   :config
+;;   (require 'flycheck-clj-kondo))
 
 (use-package cider
   :ensure t
@@ -38,9 +76,20 @@
 (use-package go-mode
   :ensure t
   :mode "\\.go\\'"
+  :init
+  (setq gofmt-command "goimports")
   :hook
   (go-mode . eglot-ensure)
   (before-save . gofmt-before-save))
+
+(use-package yaml-mode
+  :ensure t
+  :mode ("\\.yaml\\'" "\\.yml\\'"))
+
+;; (use-package eglot
+;;   :hook
+;;   (clojure-mode . eglot-ensure)
+;;   (go-mode . eglot-ensure))
 
 (use-package magit
   :ensure t)
@@ -72,16 +121,21 @@
   :init
   (vertico-mode))
 
+;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(go-mode orderless marginalia cider corfu magit paredit vertico)))
+   '(clojure-ts-mode cider clojure-mode corfu flycheck flycheck-clj-kondo go-mode magit marginalia orderless paredit vertico))
+ '(safe-local-variable-values
+   '((inf-clojure-custom-repl-type . clojure)
+     (inf-clojure-custom-startup . "clojure -A:dev"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'upcase-region 'disabled nil)
