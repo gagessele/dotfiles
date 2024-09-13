5705522eb8b10960dfc22b743bea81160df39510
Gui Gessele <gagessele@icloud.com>
2024-09-13 11:08:35 -0300

    setup script

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,9 +24,7 @@
   :custom
   ;; Support opening new minibuffers from inside existing minibuffers.
   (enable-recursive-minibuffers t)
-  ;; Emacs 28 and newer: Hide commands in M-x which do not apply to the current
-  ;; mode.  Corfu commands are hidden, since they are not used via M-x. This
-  ;; setting is useful beyond Corfu.
+  ;; Hide commands in M-x which do not apply to the current mode.
   (read-extended-command-predicate #'command-completion-default-include-p)
   ;; Enable indentation+completion using the TAB key.
   ;; `completion-at-point' is often bound to M-TAB.
@@ -53,41 +51,28 @@
     (setq insert-directory-program "gls")))
 
 (use-package exec-path-from-shell
-  :ensure t
   :config
   (exec-path-from-shell-initialize))
 
 (use-package flycheck
-  :ensure t
   :init
   (global-flycheck-mode))
 
-(use-package flycheck-clj-kondo
-  :ensure t)
+(use-package flycheck-clj-kondo)
 
 (use-package cider
-  :ensure t
   :config
   (setq cider-repl-display-help-banner nil))
 
 (use-package clojure-mode
-  :ensure t
   :config
   (require 'flycheck-clj-kondo))
 
-;; (use-package clojure-ts-mode
-;;   :ensure t
-;;   :config
-;;   (require 'flycheck-clj-kondo)
-;;   (add-hook 'clojure-ts-mode-hook #'cider-mode))
-
 (use-package corfu
-  :ensure t
   :init
   (global-corfu-mode))
 
 (use-package go-mode
-  :ensure t
   :mode "\\.go\\'"
   :init
   (setq gofmt-command "goimports")
@@ -96,14 +81,11 @@
   (before-save . gofmt-before-save))
 
 (use-package yaml-mode
-  :ensure t
   :mode ("\\.yaml\\'" "\\.yml\\'"))
 
-(use-package magit
-  :ensure t)
+(use-package magit)
 
 (use-package marginalia
-  :ensure t
   ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
   ;; available in the *Completions* buffer, add it to the
   ;; `completion-list-mode-map'.
@@ -113,19 +95,15 @@
   (marginalia-mode))
 
 (use-package orderless
-  :ensure t
   :custom
   (completion-styles '(orderless basic))
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package paredit
-  :ensure t
-  :after (clojure-mode cider)
   :hook (emacs-lisp-mode clojure-mode cider-repl-mode))
 
 (use-package vertico
-  :ensure t
   :init
   (vertico-mode))
 
@@ -135,8 +113,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(clojure-ts-mode cider clojure-mode corfu flycheck flycheck-clj-kondo go-mode magit marginalia orderless paredit vertico))
  '(safe-local-variable-values
    '((inf-clojure-custom-repl-type . clojure)
      (inf-clojure-custom-startup . "clojure -A:dev"))))
@@ -146,4 +122,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'upcase-region 'disabled nil)
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln -fns $PWD/.emacs.d/init.el $HOME/.emacs.d/init.el
+
